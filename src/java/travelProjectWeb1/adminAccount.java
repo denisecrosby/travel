596d2d0ada302423820e0f2cf43bc339d8cf29ef
denisecrosby <denisecrosby@192.168.86.23>
2018-11-30 12:00:31 -0600

    approved attractions page add view button for attraction; cleanup other admin pages

--- src/java/travelProjectWeb1/adminAccount.java
+++ src/java/travelProjectWeb1/adminAccount.java
@@ -29,10 +29,14 @@ public class adminAccount extends userAccount1 {
         Connection conn = openDatabase();
         try {
             stat = conn.createStatement();
-            rs = stat.executeQuery("Select att_id,att_name,description,cityName,stateName from attractions a, status s, state, city c where a.state_id = state.sNum and a.city_id = c.cNum "
+            rs = stat.executeQuery("Select att_id,att_name,description,cityName,stateName "
+                    + " , (select truncate(coalesce(sum(score)/count(score),0),1) from att_score where att_ID = a.att_id) as avg "
+                    + ", (case when exists(select att_id from myfavoritedes f where f.att_id = a.att_id and userName = '" + accountID + "') then 'true' else 'false' END) as favorite "
+                    + " from attractions a, status s, state, city c "
+                    + " where a.state_id = state.sNum and a.city_id = c.cNum "
                     + " and s.statusNum = a.status and s.status = '" + mark + "'");
             while (rs.next()) {
-                result.add(new attraction(rs.getString("att_id"), rs.getString("att_name"), rs.getString("description"), rs.getString("cityName"), rs.getString("stateName")));
+                result.add(new attraction(rs.getString("att_id"), rs.getString("att_name"), rs.getString("description"), rs.getString("cityName"), rs.getString("stateName"), rs.getString("favorite"), rs.getFloat("avg")));
 
             }
         } catch (SQLException e) {
